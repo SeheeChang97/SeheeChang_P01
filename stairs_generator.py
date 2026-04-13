e1f693de0d86e463ae65f9cd56f120e229d4e88e
Sehee Chang <sxc240036@utdallas.edu>
2026-04-12 23:09:40 -0500

    add handrail

--- stairs_generator.py
+++ stairs_generator.py
@@ -18,5 +18,12 @@ def create_stairs(count=10, width=3.0, height=1.0, offset=1.0, handrail=False):
         cmds.setAttr(step + ".tz", pos_z)
         
         cmds.parent(step, main_grp)
+    
+        if handrail:
+            rail = cmds.polyCylinder(r=0.1, h=2.0, name="rail_#")[0]
+            cmds.setAttr(rail + ".tx", width/2.0 - 0.2) 
+            cmds.setAttr(rail + ".ty", pos_y + 1.0)
+            cmds.setAttr(rail + ".tz", pos_z)
+            cmds.parent(rail, main_grp)
         
 create_stairs(count=12, width=4.0, handrail=True)
